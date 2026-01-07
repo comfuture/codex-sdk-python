8e2d7f7e7697f5b8f5e2584639412da8759e4fa8
comfuture <comfuture@iplateia.com>
2026-01-07 16:46:01 +0900

    Fix async abort wait handling

--- codex_sdk/asyncio/exec.py
+++ codex_sdk/asyncio/exec.py
@@ -56,6 +56,7 @@ class AsyncCodexExec:
         stderr_task = asyncio.create_task(process.stderr.read())
         abort_event: asyncio.Event | None = None
         abort_task: asyncio.Task[None] | None = None
+        abort_wait_task: asyncio.Task[bool] | None = None
         if args.signal is not None:
             abort_event = asyncio.Event()
 
@@ -68,6 +69,7 @@ class AsyncCodexExec:
                 _terminate_async_process(process)
 
             abort_task = asyncio.create_task(watch_abort())
+            abort_wait_task = asyncio.create_task(abort_event.wait())
 
         try:
             while True:
@@ -76,10 +78,10 @@ class AsyncCodexExec:
                 else:
                     line_task = asyncio.create_task(process.stdout.readline())
                     done, _pending = await asyncio.wait(
-                        {line_task, abort_event.wait()},
+                        {line_task, abort_wait_task},
                         return_when=asyncio.FIRST_COMPLETED,
                     )
-                    if abort_event.is_set():
+                    if abort_wait_task in done:
                         if not line_task.done():
                             line_task.cancel()
                             with suppress(asyncio.CancelledError):
@@ -94,7 +96,7 @@ class AsyncCodexExec:
 
             returncode = await process.wait()
             stderr_bytes = await stderr_task
-            if abort_event is not None and abort_event.is_set():
+            if abort_wait_task is not None and abort_wait_task.done():
                 raise AbortError(_abort_reason(args.signal))
             if returncode != 0:
                 stderr_text = stderr_bytes.decode("utf-8") if stderr_bytes else ""
@@ -102,6 +104,10 @@ class AsyncCodexExec:
                     f"Codex Exec exited with code {returncode}: {stderr_text}"
                 )
         finally:
+            if abort_wait_task is not None:
+                abort_wait_task.cancel()
+                with suppress(asyncio.CancelledError):
+                    await abort_wait_task
             if abort_task is not None:
                 abort_task.cancel()
                 with suppress(asyncio.CancelledError):
